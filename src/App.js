12aaef67ecf7338ffab273f2504a7a64d24cfad9
Eugeny S <dwaynehicks06@gmail.com>
2019-11-09 18:20:19 +0300

    update columns

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Aside from './components/Aside/Aside';
+import AsideNav from './components/AsideNav/AsideNav';
+import Content from './components/Content/Content';
 
 
 function App() {
   return (
     <div className="app app--chat" id="app">
       <div className="containerApp">
-        <Aside />
-        
+        <div className="app__container">
+          <AsideNav />
+          <Aside />
+          <Content />
+        </div>
       </div>
     </div>
   );
